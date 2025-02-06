086d08f7274febff5d17d3865ff212921b6027bc
Sumalya Patnala <60100821+SumalyaPatnala@users.noreply.github.com>
2025-02-06 14:17:40 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, LabelEncoder
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
-from sklearn.inspection import permutation_importance
+from scikit-learn.model_selection import train_test_split
+from scikit-learn.preprocessing import StandardScaler, LabelEncoder
+from scikit-learn.ensemble import RandomForestClassifier
+from scikit-learn.metrics import classification_report, confusion_matrix, accuracy_score
+from scikit-learn.inspection import permutation_importance
 import joblib
 import scipy.stats as stats
 
